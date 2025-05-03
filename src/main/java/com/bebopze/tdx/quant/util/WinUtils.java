bafc846ea190f80e1c1b162992a2effae9021594
bebop <718161567@qq.com>
2025-05-04 03:12:40 +0800

    tdx-quant 迁移 init  （ 原项目 tdx/tdx-script ）

---
+++ src/main/java/com/bebopze/tdx/quant/util/WinUtils.java
@@ -0,0 +1,410 @@
+package com.bebopze.tdx.quant.util;
+
+import com.sun.jna.Native;
+import com.sun.jna.platform.win32.User32;
+import com.sun.jna.platform.win32.WinDef;
+import com.sun.jna.platform.win32.WinDef.HWND;
+import com.sun.jna.platform.win32.WinUser;
+import lombok.extern.slf4j.Slf4j;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static com.bebopze.tdx.quant.util.SleepUtils.winSleep;
+
+
+/**
+ * win系统  [窗口/按钮]       -       查询/获取  /  选中/点击
+ *
+ * @author: bebopze
+ * @date: 2025/5/4
+ */
+@Slf4j
+public class WinUtils {
+
+
+    /**
+     * 按下↓ - 鼠标左键
+     */
+    private static final int WM_LBUTTONDOWN = 0x0201;
+    /**
+     * 松开↑ - 鼠标左键
+     */
+    private static final int WM_LBUTTONUP = 0x0202;
+
+
+    // -------------------------------------------- [查询 窗口/按钮] ------------------------------------------------------
+
+
+    /**
+     * 查询 [窗口]
+     *
+     * @param lpClassName  窗口 - 类名
+     * @param lpWindowName 窗口 - 标题
+     */
+    public static HWND findWindow(String lpClassName,
+                                  String lpWindowName) {
+
+        HWND hwnd = User32.INSTANCE.FindWindow(lpClassName, lpWindowName);
+
+
+        char[] windowText = new char[512];
+        char[] className = new char[512];
+
+
+        // 获取窗口标题
+        User32.INSTANCE.GetWindowText(hwnd, windowText, windowText.length);
+        String wText = Native.toString(windowText);
+
+        // 获取窗口类名
+        User32.INSTANCE.GetClassName(hwnd, className, className.length);
+        String wClassName = Native.toString(className);
+
+
+        // 过滤掉没有标题的窗口
+        if (!wText.isEmpty()) {
+            System.out.println("窗口句柄: " + hwnd);
+            System.out.println("窗口标题: " + wText);
+            System.out.println("窗口类名: " + wClassName);
+            System.out.println("----------");
+        }
+
+
+        return hwnd;
+    }
+
+
+    /**
+     * 查询 [窗口-按钮]
+     *
+     * @param lpClassName  窗口 - 类名
+     * @param lpWindowName 窗口 - 标题
+     * @param buttonTitle  窗口 - 按钮标题
+     * @return
+     * @throws Exception
+     */
+    public static HWND findWindowsButton(String lpClassName,
+                                         String lpWindowName,
+                                         String buttonTitle) {
+
+
+        HWND hWnd = User32.INSTANCE.FindWindow(lpClassName, lpWindowName);
+        if (hWnd != null) {
+            System.out.println("找到窗口：[" + lpWindowName + "]");
+
+
+            HWND hButton = User32.INSTANCE.FindWindowEx(hWnd, null, null, buttonTitle);
+            if (hButton != null) {
+                System.out.println("找到按钮：[" + lpWindowName + "-" + buttonTitle + "]");
+
+                System.out.println("---");
+                return hButton;
+            }
+
+
+            System.out.println("未找到按钮：[" + lpWindowName + "-" + buttonTitle + "]");
+            System.out.println("---");
+            return null;
+        }
+
+
+        System.out.println("未找到窗口：[" + lpWindowName + "]");
+        System.out.println("---");
+        return null;
+    }
+
+
+    // -------------------------------------------- [切换 窗口/按钮] ------------------------------------------------------
+
+
+    /**
+     * 窗口切换     -     通过 [窗口类名/窗口标题]   查询并切换 窗口
+     *
+     * @param lpClassName  窗口类名
+     * @param lpWindowName 窗口标题
+     */
+    public static void windowSwitcher(String lpClassName,
+                                      String lpWindowName) {
+
+        // 查询窗口
+        HWND hwnd = findWindow(lpClassName, lpWindowName);
+
+
+        // 切换窗口
+        windowSwitcher(hwnd);
+    }
+
+
+    /**
+     * 窗口切换（选中 -> 窗口）
+     *
+     * @param hwnd 窗口
+     */
+    public static void windowSwitcher(HWND hwnd) {
+
+        if (hwnd != null) {
+            // 将窗口 置于前台
+            User32.INSTANCE.SetForegroundWindow(hwnd);
+
+
+            // win系统 反应时间
+            winSleep();
+        }
+    }
+
+
+    public static void test01(HWND hwnd) {
+        if (hwnd == null) {
+            System.out.println("未找到窗口");
+            return;
+        }
+
+
+        // 最小化 [窗口]
+        minimizesWindow(hwnd);
+
+        // 最大化 [窗口]
+        maximizesWindow(hwnd);
+
+        // 关闭 [窗口]
+        closeWindow(hwnd);
+    }
+
+
+    // ---------------------------------------- [窗口 最小化/最大化/关闭] --------------------------------------------------
+
+
+    /**
+     * 最小化 [窗口]
+     *
+     * @param hwnd
+     */
+    public static void minimizesWindow(HWND hwnd) {
+        if (hwnd == null) {
+            System.out.println("未找到窗口");
+            return;
+        }
+
+
+        // 发送 [最小化]消息
+        User32.INSTANCE.PostMessage(hwnd, WinUser.WM_SYSCOMMAND, new WinDef.WPARAM(WinUser.SC_MINIMIZE), new WinDef.LPARAM(0));
+        System.out.println("---------------------------- 最小化");
+        winSleep();
+    }
+
+
+    /**
+     * 最大化 [窗口]
+     *
+     * @param hwnd
+     */
+    public static void maximizesWindow(HWND hwnd) {
+        if (hwnd == null) {
+            System.out.println("未找到窗口");
+            return;
+        }
+
+
+        // 发送 [最大化]消息
+        User32.INSTANCE.PostMessage(hwnd, WinUser.WM_SYSCOMMAND, new WinDef.WPARAM(WinUser.SC_MAXIMIZE), new WinDef.LPARAM(0));
+        System.out.println("---------------------------- 最大化");
+        winSleep();
+    }
+
+
+    /**
+     * 关闭 [窗口]
+     *
+     * @param hwnd
+     */
+    public static void closeWindow(HWND hwnd) {
+        if (hwnd == null) {
+            System.out.println("未找到窗口");
+            return;
+        }
+
+
+        // 发送 [关闭]消息
+        User32.INSTANCE.SendMessageTimeout(hwnd, WinUser.WM_CLOSE, null, null, WinUser.SMTO_ABORTIFHUNG, 50, null);
+        System.out.println("---------------------------- 关闭");
+
+
+        // 需 大于   [msg] - timeout
+        winSleep();
+    }
+
+
+    /**
+     * 关闭 [窗口]
+     *
+     * @param hwnd
+     */
+    @Deprecated
+    public static void closeWindow2(HWND hwnd) {
+        if (hwnd == null) {
+            System.out.println("未找到窗口");
+            return;
+        }
+
+
+        // 发送 [关闭]消息
+        User32.INSTANCE.PostMessage(hwnd, WinUser.WM_SYSCOMMAND, new WinDef.WPARAM(WinUser.WM_CLOSE), new WinDef.LPARAM(0));
+        System.out.println("---------------------------- 关闭");
+
+
+        winSleep();
+    }
+
+
+    // ------------------------------------------------ [键盘] ----------------------------------------------------------
+
+
+    /**
+     * 键盘输入
+     *
+     * @param keyList [按键] 列表
+     */
+    public static void keyPress(List<Integer> keyList) {
+
+
+        try {
+            Robot robot = new Robot();
+
+            // .902
+            for (Integer key : keyList) {
+                robot.keyPress(key);
+                winSleep(100);
+            }
+
+
+            // [回车] -> 确认
+            robot.keyPress(KeyEvent.VK_ENTER);
+
+
+            log.info("键盘输入  :  [{}]     ->     suc", keyList);
+
+
+        } catch (Exception e) {
+
+            log.error("键盘输入  :  [{}]     ->     fail : {}", keyList, e.getMessage(), e);
+        }
+
+
+        winSleep();
+    }
+
+
+    // ------------------------------------------------ [鼠标] ----------------------------------------------------------
+
+
+    /**
+     * 鼠标左键  -  点击[按钮] -> 松开[按钮]
+     *
+     * @param hwnd 窗口/按钮
+     */
+    public static void clickMouseLeft(HWND hwnd) {
+
+
+        // 你可以使用 SendMessage/PostMessage     来模拟点击等操作
+
+
+        /**
+         * https://learn.microsoft.com/zh-cn/windows/win32/api/winuser/nf-winuser-sendmessage
+         *
+         *
+         * https://learn.microsoft.com/zh-cn/windows/win32/winmsg/about-messages-and-message-queues#message-types
+         *
+         * https://learn.microsoft.com/zh-cn/windows/win32/controls/bumper-button-control-reference-notifications
+         *
+         *
+         * ------ https://learn.microsoft.com/zh-cn/windows/win32/inputdev/mouse-input-notifications
+         * ------ https://zhiyong.wang/archives/131#0x04-%E4%BD%BF%E7%94%A8%E5%8F%A5%E6%9F%84%E6%93%8D%E4%BD%9C%E7%AA%97%E5%8F%A3
+         * ------ https://www.cnblogs.com/co.902
+         * de1992/p/11239881.html
+         *
+         * --
+         * -- https://stackoverflow.com/questions/5713730/c-sharp-press-a-button-from-a-window-using-user32-dll
+         */
+
+
+        // int MK_LBUTTON = 0x0001;
+        // WinDef.WPARAM wparam = new WinDef.WPARAM(MK_LBUTTON);
+
+
+        // 点击 [按钮]
+        User32.INSTANCE.SendMessageTimeout(hwnd, WM_LBUTTONDOWN, null, null, WinUser.SMTO_ABORTIFHUNG, 50, null);
+        System.out.println("鼠标左键 - 点击 [按钮]");
+
+
+        // 需 大于   [msg] - timeout
+        winSleep();
+
+
+        // 松开 [按钮]
+        User32.INSTANCE.SendMessageTimeout(hwnd, WM_LBUTTONUP, null, null, WinUser.SMTO_ABORTIFHUNG, 50, null);
+        System.out.println("鼠标左键 - 松开 [按钮]");
+
+
+        // 需 大于   [msg] - timeout
+        winSleep();
+    }
+
+
+    // ------------------------------------------------ [cmd] ----------------------------------------------------------
+
+
+    /**
+     * 打开App
+     *
+     * @param appPath app路径
+     */
+    public static void openApp(String appPath) {
+
+        try {
+            // 打开App
+            Process process = Runtime.getRuntime().exec(appPath);
+
+
+            // 等待5s
+            process.waitFor(5, TimeUnit.SECONDS);
+
+
+            log.info("---------------------------- 打开App [{}]", appPath);
+
+
+        } catch (Exception e) {
+
+            log.error("openApp - fail     >>>     appPath : {}   ，  errMsg : {}   ,   ex : ", appPath, e.getMessage(), e);
+        }
+    }
+
+
+    /**
+     * kill App 进程
+     *
+     * @param exeName 进程名称     如：tdxw.exe
+     */
+    public static void killApp(String exeName) {
+
+        // 获取 app进程  并关闭
+
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder("taskkill", "/IM", exeName, "/F");
+            processBuilder.start();
+
+            log.info("关闭App   ->   suc   ------------------------------");
+
+
+        } catch (IOException e) {
+
+            log.error("关闭App   ->   异常 : {}", e.getMessage(), e);
+        }
+
+
+        winSleep();
+    }
+
+}
---
+++ src/main/java/com/bebopze/tdx/quant/util/WinUtils3.java
@@ -0,0 +1,367 @@
+package com.bebopze.tdx.quant.util;
+
+import com.google.common.collect.Lists;
+import com.sun.jna.platform.win32.User32;
+import com.sun.jna.platform.win32.WinDef.HWND;
+import com.sun.jna.platform.win32.WinUser;
+import lombok.extern.slf4j.Slf4j;
+
+import java.awt.*;
+import java.awt.event.InputEvent;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import static java.awt.event.KeyEvent.*;
+
+
+/**
+ * Win系统   -   鼠标坐标
+ *
+ * @author: bebopze
+ * @date: 2025/5/4
+ */
+@Slf4j
+@Deprecated
+public class WinUtils3 {
+
+
+    private static final int WM_LBUTTONDOWN = 0x0201;
+    private static final int WM_LBUTTONUP = 0x0202;
+
+
+    public static void main(String[] args) throws Exception {
+
+
+        openAndCloseWinApp();
+
+
+        // Thread.sleep(3000);
+
+
+        // getWindowsButton2();
+
+
+        // [窗口-按钮] - 坐标
+        winButtonCoordinate();
+
+
+        // openAndCloseMacApp();
+
+        // sshWin();
+    }
+
+
+    public static void openAndCloseWinApp() {
+
+
+        // 打开App
+        WinUtils.openApp("tdx path");
+
+
+        // 获取窗口
+
+
+        // 键盘输入   ->   [.902]
+        ArrayList<Integer> list = Lists.newArrayList(VK_PERIOD, VK_9, VK_0, VK_2);
+        WinUtils.keyPress(list);
+
+
+        // 获取当前 鼠标-[坐标点]
+        getMousePointerInfo();
+
+
+        // 移动鼠标 - 指定[坐标点]
+        mouseMove(2078, 702);
+
+
+        // 获取当前 鼠标-[坐标点]
+        getMousePointerInfo();
+
+
+        // 点击鼠标 - 左键
+        clickMouseLeft2();
+
+
+        // 关闭App
+        // closeApp();
+    }
+
+
+    /**
+     * 移动鼠标   ->   指定 [坐标点]
+     *
+     * @param x
+     * @param y
+     */
+    public static void mouseMove(int x,
+                                 int y) {
+
+        try {
+            Robot robot = new Robot();
+            robot.delay(1000);
+
+
+            Point point1 = new Point(1, 1).getLocation();
+            Point point2 = new Point(0, 0).getLocation();
+            Point point3 = new Point(x, y).getLocation();
+
+
+            robot.mouseMove(point1.x, point1.y); // 将鼠标移动到屏幕的(100, 100)位置
+            Thread.sleep(500);
+            getMousePointerInfo();
+
+            robot.mouseMove(point2.x, point2.y);
+            Thread.sleep(500);
+            getMousePointerInfo();
+
+            robot.mouseMove(point3.x, point3.y);
+            getMousePointerInfo();
+
+
+            log.info("鼠标移动  :  [{},{}]     ->     suc   ------------------------------", x, y);
+
+
+            Thread.sleep(2000);
+
+
+        } catch (Exception e) {
+            log.error("鼠标移动   ->   异常 : {}", e.getMessage());
+        }
+    }
+
+
+    /**
+     * 点击鼠标 - 左键
+     */
+    public static void clickMouseLeft2() {
+
+
+        try {
+            Robot robot = new Robot();
+            robot.delay(1000);
+
+
+            // 按下鼠标左键
+            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+            robot.delay(50);
+
+            // 释放鼠标左键
+            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+
+
+            log.info("鼠标点击   ->   suc   ------------------------------");
+            getMousePointerInfo();
+
+
+            Thread.sleep(500);
+
+
+        } catch (Exception e) {
+            log.error("鼠标点击   ->   异常 : {}", e.getMessage());
+        }
+    }
+
+    /**
+     * 获取当前 鼠标的 坐标点
+     */
+    public static void getMousePointerInfo() {
+
+
+        PointerInfo pointerInfo = MouseInfo.getPointerInfo();
+        Point point = pointerInfo.getLocation();
+
+
+        // [全部刷新] 坐标   -   [2143, 730]
+        int x = point.x;
+        int y = point.y;
+
+
+        log.info("鼠标位置 : [{},{}]   ->   suc   ------------------------------", x, y);
+    }
+
+
+    /**
+     * [窗口-按钮] - 坐标
+     */
+    public static void winButtonCoordinate() throws Exception {
+
+        // 获取  [窗口] - [按钮]
+        HWND hButton = WinUtils.findWindowsButton(null, "扩展数据管理器", "全部刷新");
+
+
+        if (hButton != null) {
+
+            WinUser.RECT rect = new WinUser.RECT();
+            User32.INSTANCE.GetWindowRect(hButton, rect);
+
+
+            int x = rect.left;
+            int y = rect.top;
+            int width = rect.right - rect.left;
+            int height = rect.bottom - rect.top;
+
+            System.out.println("Button Coordinates: (" + x + ", " + y + "), Width: " + width + ", Height: " + height);
+        } else {
+            System.out.println("Button not found");
+        }
+    }
+
+
+//    public static void openApp2(String appPath) throws Exception {
+//
+//
+//        // 获取Mac应用程序的可执行文件路径
+//        // String appPath = "/Applications/YourApp.app/Contents/MacOS/YourApp";
+//
+//        // 构建执行命令的进程
+//        ProcessBuilder processBuilder = new ProcessBuilder(appPath);
+//
+//        // 启动进程
+//        Process process = processBuilder.start();
+//
+//        // 获取进程的输出流
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+//        String line;
+//        while ((line = reader.readLine()) != null) {
+//            // 处理应用程序的输出信息
+//            System.out.println(line);
+//        }
+//
+//        // 等待进程执行结束
+//        int exitCode = process.waitFor();
+//        System.out.println("应用程序执行结束，退出码：" + exitCode);
+//    }
+
+
+    //    /**
+//     * [窗口-按钮] - 坐标
+//     */
+//    public static void winButtonCoordinate() throws Exception {
+//
+//        // 获取  [窗口] - [按钮]
+//        HWND hButton = getWindowsButton(null, "扩展数据管理器", "全部刷新");
+//
+//
+//        if (hButton != null) {
+//
+//            WinUser.RECT rect = new WinUser.RECT();
+//            User32.INSTANCE.GetWindowRect(hButton, rect);
+//
+//
+//            int x = rect.left;
+//            int y = rect.top;
+//            int width = rect.right - rect.left;
+//            int height = rect.bottom - rect.top;
+//
+//            System.out.println("Button Coordinates: (" + x + ", " + y + "), Width: " + width + ", Height: " + height);
+//        } else {
+//            System.out.println("Button not found");
+//        }
+//    }
+
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+
+    public static void openAndCloseMacApp() throws Exception {
+
+        // 打开 Safari
+        System.out.println("Opening Safari...");
+        Runtime.getRuntime().exec("open -a Safari");
+
+        // 等待 5 秒后关闭 Safari
+        Thread.sleep(5000);
+
+        // 使用 AppleScript 优雅关闭 Safari
+        System.out.println("Closing Safari...");
+        Runtime.getRuntime().exec("killall Safari");
+        // Runtime.getRuntime().exec("osascript -e 'quit app \"Safari\"'");
+    }
+
+
+    public static void openAndCloseWinApp2() throws Exception {
+
+
+        // 打开记事本
+        System.out.println("Opening tdx...");
+        Runtime.getRuntime().exec("/Volumes/[C] Windows 11.hidden/soft/通达信/v_2024/new_tdx/tdxw.exe");
+
+        // 等待 5 秒后关闭记事本
+        Thread.sleep(5000);
+
+        // 关闭记事本
+        System.out.println("Closing tdx...");
+        Runtime.getRuntime().exec("taskkill /F /IM tdxw.exe");
+    }
+
+
+    public static void test01() throws Exception {
+
+
+        //
+        Process exec = Runtime.getRuntime().exec("/Applications/NetEaseMusic.app/Contents/MacOS/NetEaseMusic");
+
+
+        // 等待进程执行结束
+//        int exitCode = exec.waitFor();
+//        System.out.println("应用程序执行结束，退出码：" + exitCode);
+
+
+//        InputStream is = exec.getInputStream();
+//
+//        int line;
+//        while ((line = is.read()) != 0) {
+//            // 处理应用程序的输出信息
+//            System.out.println(line);
+//        }
+
+
+//        Thread.sleep(5000L);
+        System.out.println("打开 Mac电脑💻   网易云音乐🎵App");
+
+
+//        Robot robot = new Robot();
+//        robot.delay(5000);
+
+//        //先模拟输入主方法
+//        robot.keyPress(KeyEvent.VK_P);
+//        robot.keyRelease(KeyEvent.VK_P);
+//        robot.delay(100);
+
+//        robot.keyPress(KeyEvent.VK_S);
+//        robot.keyRelease(KeyEvent.VK_S);
+//        robot.delay(100);
+
+//        robot.keyPress(KeyEvent.VK_V);
+//        robot.keyRelease(KeyEvent.VK_V);
+//        robot.delay(100);
+
+//        robot.keyPress(KeyEvent.VK_M);
+//        robot.keyRelease(KeyEvent.VK_M);
+
+    }
+
+
+    public static void sshWin() throws Exception {
+
+        String windowsIp = "192.168.0.1";  // Windows 虚拟机的 IP 地址
+        String username = "xxx"; // Windows 虚拟机的用户名
+        String password = ""; // Windows 虚拟机的密码
+        String command = "C:\\soft\\通达信\\v_2024\\new_tdx\\tdxw.exe";    // 远程执行的命令
+
+        // 构建 SSH 命令
+        String sshCommand = String.format("ssh %s@%s %s", username, windowsIp, command);
+
+        // 执行 SSH 命令
+        Process process = Runtime.getRuntime().exec(sshCommand);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+
+    }
+
+}
