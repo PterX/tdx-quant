ea5d5e96c25c16d005d36c722f4049e6612ded2b
bebop <718161567@qq.com>
2025-05-06 19:08:54 +0800

    block_gn.dat（概念板块-个股列表） / block_fg.dat（风格板块- 个股列表） / block_zs.dat（指数-个股列表） - 解析

---
+++ src/main/java/com/bebopze/tdx/quant/tdxdata/BlockGnParser.java
@@ -0,0 +1,247 @@
+package com.bebopze.tdx.quant.tdxdata;
+
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Maps;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.compress.utils.Lists;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+import static com.bebopze.tdx.quant.common.constant.TdxConst.TDX_PATH;
+
+
+/**
+ * block_gn.dat / block_fg.dat / block_zs.dat   -   解析
+ * -
+ * -   /T0002/hq_cache/block_gn.dat               概念板块 - 个股列表
+ * -   /T0002/hq_cache/block_fg.dat               风格板块 - 个股列表
+ * -   /T0002/hq_cache/block_zs.dat               指数    - 个股列表
+ *
+ * @author: bebopze
+ * @date: 2025/5/6
+ */
+@Slf4j
+public class BlockGnParser {
+
+
+    // TDX 缓存相对用户主目录的路径
+    private static final String BASE_PATH = TDX_PATH + "/T0002/hq_cache/";
+
+
+    // ---------------------------------------------------------------------------
+    private static final String filePath_gn = TDX_PATH + "/T0002/hq_cache/block_gn.dat";
+
+
+    public static void main(String[] args) {
+
+        parse("gn");
+        // parse("fg");
+        // parse("zs");
+
+
+        System.out.println();
+    }
+
+
+    /**
+     * 解析“gn”等板块：加载 .cfg 和 .dat 并合并
+     *
+     * @param blk ->   gn / fg / zs
+     */
+    @SneakyThrows
+    public static void parse(String blk) {
+
+
+        // 读取解析
+        List<BlockDatDTO> blockDatDTOList = execParse(blk);
+
+
+//        // 异常数据 - 过滤
+//        Set<String> del = Sets.newHashSet();
+//        if ("gn".equals(blk)) {
+//            del.add("��GDR");
+//        } else if ("fg".equals(blk)) {
+//            del.add("����ͨSH");
+//            del.add("���ͨSZ");
+//            del.add("������ȯ");
+//        }
+//
+//        Iterator<BlockgnParser.BlockFile> it = blockFileList.iterator();
+//        while (it.hasNext()) {
+//            if (del.contains(it.next().name)) {
+//                it.remove();
+//            }
+//        }
+
+
+        // 板块name - 板块code
+        Map<String, String> name_code_map = name_code_map(blk);
+        System.out.println(JSON.toJSONString(name_code_map));
+
+
+        // 补齐   板块code
+        for (BlockDatDTO block : blockDatDTOList) {
+
+            // 锂电池概念 - 锂电池
+            // 核污染防治 - 核污防治
+            // 车路云（tdxzs3.cfg 不存在）   -   车联网（？？？）
+            name_code_map.forEach((name, code) -> {
+
+                if (name.contains(block.name)) {
+                    block.setCode(code);
+                }
+            });
+
+
+            if (block.code == null) {
+                log.warn("BlockGnParser#parse warn     >>>     name : {} , block : {}", block.name, JSON.toJSONString(block));
+            }
+        }
+    }
+
+
+    /**
+     * 读取二进制  block_<blk>.dat   并解析每个板块条目
+     *
+     * @param blk ->   gn / fg / zs
+     * @return
+     * @throws IOException
+     */
+    public static List<BlockDatDTO> execParse(String blk) throws IOException {
+
+        byte[] buff = Files.readAllBytes(Paths.get(BASE_PATH + "block_" + blk + ".dat"));
+        ByteBuffer bbCount = ByteBuffer.wrap(buff, 384, 2).order(ByteOrder.LITTLE_ENDIAN);
+
+
+        // 板块 总数
+        int blockCount = bbCount.getShort();
+
+
+        List<BlockDatDTO> list = Lists.newArrayList();
+
+
+        int offset = 386;
+        for (int i = 0; i < blockCount; i++) {
+            int start = offset + i * 2813;
+            byte[] blockBytes = Arrays.copyOfRange(buff, start, start + 2813);
+
+
+            // 板块名称
+            String name = new String(blockBytes, 0, 8, "GBK").trim();
+            ByteBuffer bb = ByteBuffer.wrap(blockBytes, 9, 4).order(ByteOrder.LITTLE_ENDIAN);
+
+
+            // 板块  -  关联 个股数量
+            int stockNum = bb.getShort();
+
+
+            // 板块  -  关联 个股code列表
+            List<String> stockCodeList = Lists.newArrayList();
+            for (int j = 0; j < stockNum; j++) {
+                String stockCode = new String(blockBytes, 13 + 7 * j, 7, "GBK").trim();
+                if (!stockCode.isEmpty()) {
+                    stockCodeList.add(stockCode);
+                }
+            }
+
+
+            list.add(new BlockDatDTO(null, name, blk, stockNum, stockCodeList));
+        }
+
+
+        return list;
+    }
+
+
+    /**
+     * 解析 tdxzs3.cfg，返回指定板块类型的列表
+     */
+    public static Map<String, String> name_code_map(String blk) throws IOException {
+        Map<String, String> name_code_map = Maps.newHashMap();
+
+
+        List<Tdxzs3Parser.Tdxzs3DTO> dtoList = Tdxzs3Parser.parse();
+
+
+        Map<String, String> mapping = Maps.newHashMap();
+        // 普通行业（细分行业）
+        mapping.put("hy", "2");
+        // 地区
+        mapping.put("dq", "3");
+        // 概念
+        mapping.put("gn", "4");
+        // 风格
+        mapping.put("fg", "5");
+        // 研究行业
+        mapping.put("yjhy", "12");
+        // 指数
+        mapping.put("zs", "6");
+        String type = mapping.get(blk);
+
+
+        dtoList.stream().filter(e -> e.getBlockType().equals(type)).forEach(e -> {
+
+            // if ("gn".equals(blk)) {
+
+            // 锂电池概念 - 锂电池
+            // 核污染防治 - 核污防治
+            name_code_map.put(e.name + "|" + e.TXCode, e.code);
+
+
+            // 车路云（tdxzs3.cfg 不存在）   ->   车联网（？？？）
+            if ("车联网".equals(e.name)) {
+                name_code_map.put(e.name + "|车路云", e.code);
+            }
+
+
+            // } else {
+            //
+            //    // 活跃可转债|880677|5|2|0|活跃转债          5-风格板块
+            //    name_code_map.put(e.name, e.code);
+            // }
+        });
+
+
+        return name_code_map;
+    }
+
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+
+    @Data
+    @AllArgsConstructor
+    static class BlockDatDTO {
+        /**
+         * 板块-name
+         */
+        String name;
+        /**
+         * 板块-code
+         */
+        String code;
+
+        /**
+         * 板块类型：gn-概念；fg-风格；zs-指数；
+         */
+        String type;
+
+        /**
+         * 板块 - 个股数量
+         */
+        int num;
+        /**
+         * 板块 - 个股code列表
+         */
+        List<String> stockCodeList;
+    }
+
+}
