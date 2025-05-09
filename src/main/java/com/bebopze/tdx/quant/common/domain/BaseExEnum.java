48c9e3e190102b6bceb3ee44e5efe16df8874f8b
bebop <718161567@qq.com>
2025-05-09 10:17:26 +0800

    Result

---
+++ src/main/java/com/bebopze/tdx/quant/common/domain/BaseExEnum.java
@@ -0,0 +1,41 @@
+package com.bebopze.tdx.quant.common.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+
+/**
+ * 异常
+ *
+ * @author: bebopze
+ * @date: 2025/5/6
+ */
+@AllArgsConstructor
+public enum BaseExEnum {
+
+
+    SUC(200, "success"),
+
+
+    ERR(500, "系统异常"),
+
+
+    NOT_LOGIN(301, "操作会话已失效，请重新登录！"),
+
+    NOT_AUTH(302, "您无该权限"),
+
+
+    API_404(404, "该接口不存在");
+
+
+    @Getter
+    @Setter
+    private Integer code;
+
+    @Getter
+    @Setter
+    private String msg;
+
+}
---
+++ src/main/java/com/bebopze/tdx/quant/common/domain/Result.java
@@ -0,0 +1,161 @@
+package com.bebopze.tdx.quant.common.domain;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+
+/**
+ * 统一返回 Result类
+ *
+ * @author: bebopze
+ * @date: 2025/5/7
+ */
+@Data
+public class Result<T> implements Serializable {
+
+    private static final long serialVersionUID = -2361820086956983473L;
+
+
+    // 数据明细
+    private T data;
+
+    private Boolean success;
+
+    private Integer code;
+
+    private String msg;
+
+
+    /**
+     * 为null时,不参与序列化
+     */
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Integer totalNum;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Integer pageIndex;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Integer pageSize;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Integer totalPage;
+
+
+    public static <T> Result of(T data,
+                                boolean success,
+                                BaseExEnum baseExEnum) {
+        if (null != baseExEnum) {
+            return of(data, success, baseExEnum.getCode(), baseExEnum.getMsg());
+        } else {
+            return of(data, success, null, null);
+        }
+    }
+
+    public static <T> Result of(T data,
+                                boolean success,
+                                Integer code,
+                                String msg) {
+        Result result = new Result<>();
+        result.setData(data);
+        result.setSuccess(success);
+        result.setCode(code);
+        result.setMsg(msg);
+        if (data instanceof Collection) {
+            result.setTotalNum(((Collection) data).size());
+        }
+        return result;
+    }
+
+    public static <T> Result<T> of(T data,
+                                   boolean success,
+                                   BaseExEnum baseExEnum,
+                                   Integer totalNum,
+                                   Integer pageIndex,
+                                   Integer pageSize) {
+        return of(data, success, baseExEnum, totalNum, pageIndex, pageSize, null);
+    }
+
+    public static <T> Result<T> of(T data,
+                                   boolean success,
+                                   BaseExEnum baseExEnum,
+                                   Integer totalNum,
+                                   Integer pageIndex,
+                                   Integer pageSize,
+                                   String msg) {
+        Result result = new Result();
+        result.setData(data);
+        result.setSuccess(success);
+        if (null != baseExEnum) {
+            result.setCode(baseExEnum.getCode());
+            result.setMsg(baseExEnum.getMsg());
+        }
+        if (data instanceof Collection && null == totalNum) {
+            result.setTotalNum(((Collection) data).size());
+        }
+        result.setTotalNum(totalNum);
+        result.setPageIndex(pageIndex);
+        result.setPageSize(pageSize);
+        result.setTotalPage((pageSize == null || pageSize == 0) ? null : (totalNum % pageSize == 0 ? totalNum / pageSize : (totalNum / pageSize + 1)));
+        result.setMsg(msg);
+        return result;
+    }
+
+
+    public static <T> Result<T> SUC() {
+        return SUC(null);
+    }
+
+    public static <T> Result<T> SUC(T data) {
+        return of(data, true, BaseExEnum.SUC);
+    }
+
+    public static <T> Result<T> SUC(T data,
+                                    Integer totalNum,
+                                    Integer pageIndex,
+                                    Integer pageSize,
+                                    String msg) {
+        return of(data, true, BaseExEnum.SUC, totalNum, pageIndex, pageSize, msg);
+    }
+
+    public static <T> Result<T> SUC(T data,
+                                    Integer totalNum,
+                                    Integer pageIndex,
+                                    Integer pageSize) {
+        return of(data, true, BaseExEnum.SUC, totalNum, pageIndex, pageSize);
+    }
+
+    public static <T> Result<T> SUC(T data,
+                                    BaseExEnum baseExEnum) {
+        return of(data, true, baseExEnum);
+    }
+
+    public static <T> Result<T> SUC(T data,
+                                    String msg) {
+        return of(data, true, BaseExEnum.SUC.getCode(), msg);
+    }
+
+    public static Result ERR(BaseExEnum baseExEnum) {
+        Result result = new Result();
+        result.setSuccess(false);
+        if (null != baseExEnum) {
+            result.setCode(baseExEnum.getCode());
+            result.setMsg(baseExEnum.getMsg());
+        }
+        result.setData(null);
+        return result;
+    }
+
+    public static Result ERR(String msg) {
+        Result result = new Result();
+        result.setSuccess(false);
+        result.setCode(BaseExEnum.ERR.getCode());
+        result.setMsg(msg);
+        result.setData(null);
+        return result;
+    }
+}
+
