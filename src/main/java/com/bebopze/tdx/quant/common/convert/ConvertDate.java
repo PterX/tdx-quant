b99042bd9b47846f268ead02f8f049adc76c6e01
bebop <718161567@qq.com>
2025-05-31 12:10:40 +0800

    ConvertDate   ==>   倒序 idx（今日  ->  上市 第1天）

--- src/main/java/com/bebopze/tdx/quant/common/convert/ConvertDate.java
+++ src/main/java/com/bebopze/tdx/quant/common/convert/ConvertDate.java
@@ -5,6 +5,7 @@ import com.bebopze.tdx.quant.common.util.DateTimeUtil;
 import com.bebopze.tdx.quant.common.util.MybatisPlusUtil;
 import com.bebopze.tdx.quant.dal.entity.BaseBlockDO;
 import com.bebopze.tdx.quant.dal.mapper.BaseBlockMapper;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 import java.time.LocalDate;
@@ -23,30 +24,79 @@ import static com.bebopze.tdx.quant.common.constant.TdxConst.INDEX_BLOCK;
 public class ConvertDate {
 
 
-    public static final Map<String, Integer> dateIndexMap = Maps.newHashMap();
+    /**
+     * 倒序 idx（今日  ->  上市 第1天）
+     */
+    public static final Map<String, Integer> reverse__dateIndexMap = Maps.newHashMap();
+
+    private static boolean initialized = false;
 
 
-    static {
-        BaseBlockMapper mapper = MybatisPlusUtil.getMapper(BaseBlockMapper.class);
-        BaseBlockDO baseBlockDO = mapper.getByCode(INDEX_BLOCK);
+    private static synchronized void init() {
+        if (initialized) return;
 
 
-        if (baseBlockDO != null) {
+        try {
+            BaseBlockMapper mapper = MybatisPlusUtil.getMapper(BaseBlockMapper.class);
+            BaseBlockDO baseBlockDO = mapper.getByCode(INDEX_BLOCK);
 
-            List<KlineDTO> klineDTOList = ConvertStockKline.klineHis2DTOList(baseBlockDO.getKlineHis());
-            String[] date_arr = ConvertStockKline.strFieldValArr(klineDTOList, "date");
+            if (baseBlockDO != null) {
+                List<KlineDTO> klineDTOList = ConvertStockKline.klineHis2DTOList(baseBlockDO.getKlineHis());
+                String[] date_arr = ConvertStockKline.strFieldValArr(klineDTOList, "date");
 
 
-            for (int i = 0; i < date_arr.length; i++) {
-                dateIndexMap.put(date_arr[i], i);
+                // 倒序 idx（今日  ->  上市 第1天）
+                for (int i = date_arr.length - 1; i >= 0; i--) {
+                    reverse__dateIndexMap.put(date_arr[i], i);
+                }
             }
+
+
+            initialized = true;
+
+        } catch (Exception e) {
+            throw new RuntimeException("ConvertDate 初始化失败", e);
         }
     }
 
 
     public static boolean getByDate(boolean[] arr, LocalDate date) {
-        int idx = dateIndexMap.get(DateTimeUtil.format_yyyy_MM_dd(date));
+        init(); // 第一次使用时初始化
+
+        // 倒序 idx
+        int reverse_idx = reverse__dateIndexMap.get(DateTimeUtil.format_yyyy_MM_dd(date));
+        // 正序 idx
+        int idx = arr.length - reverse_idx - 1;
+
         return arr[idx];
+    }
+
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+
+    public static void main(String[] args) {
+
+
+        List<Integer> list = Lists.newArrayList(1, 2, 3, 4, 5);
+
+
+        Map<Integer, Integer> reverse_idxMap = Maps.newHashMap();
+
+
+        for (int i = list.size() - 1; i >= 0; i--) {
+            // 倒序 idx
+            reverse_idxMap.put(i, list.get(i));
+        }
+
+
+        for (Integer reverse_idx : reverse_idxMap.keySet()) {
+            // 正序 idx
+            int idx = list.size() - reverse_idx - 1;
+            Integer val = list.get(idx);
+
+            System.out.println(reverse_idx + "  " + idx + "     " + val);
+        }
     }
 
 }
